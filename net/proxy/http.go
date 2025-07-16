9f8fd7f5278b5a77f59bc175ae882cf9186a3d6f
fengnianwang <fengnianwang@thinredline.com.cn>
2025-07-16 11:16:21 +0800

    debug

--- net/proxy/http.go
+++ net/proxy/http.go
@@ -40,8 +40,16 @@ func (r *Request) URL() string {
 
 type Response struct {
 	StatusCode int
-	Header     map[string]string
-	Body       any
+	Header     map[string]any
+	Body       io.ReadCloser
+}
+
+func (resp *Response) Encode() (map[string]any, error) {
+	return map[string]any{
+		"StatusCode": resp.StatusCode,
+		"Header":     resp.Header,
+		"Body":       resp.Body,
+	}, nil
 }
 
 func errorResponse(err error) *Response {
@@ -50,9 +58,9 @@ func errorResponse(err error) *Response {
 	}
 	return &Response{
 		StatusCode: http.StatusInternalServerError,
-		Body: map[string]string{
-			"error": throw(err).Error(),
-		},
+		//Body: map[string]string{
+		//	"error": throw(err).Error(),
+		//},
 	}
 }
 
@@ -74,18 +82,24 @@ func (p *proxy) Run(ctx context.Context, input any) (any, error) {
 	if err := plugin.DecodeInput(input, &req); err != nil {
 		return nil, throw(err)
 	}
-	buf := bytes.NewBuffer(nil)
-	switch {
-	case strings.Contains(req.Header["Content-Type"], "application/json"):
-		if err := json.NewEncoder(buf).Encode(req.Body); err != nil {
-			return nil, throw(err)
+	var reqBody io.Reader
+	if b, ok := req.Body.(io.Reader); ok {
+		reqBody = b
+	} else {
+		buf := bytes.NewBuffer(nil)
+		switch {
+		case strings.Contains(req.Header["Content-Type"], "application/json"):
+			if err := json.NewEncoder(buf).Encode(req.Body); err != nil {
+				return nil, throw(err)
+			}
+		case strings.Contains(req.Header["Content-Type"], "application/x-yaml"):
+			if err := yaml.NewEncoder(buf).Encode(req.Body); err != nil {
+				return nil, throw(err)
+			}
 		}
-	case strings.Contains(req.Header["Content-Type"], "application/x-yaml"):
-		if err := yaml.NewEncoder(buf).Encode(req.Body); err != nil {
-			return nil, throw(err)
-		}
+		reqBody = buf
 	}
-	r, err := http.NewRequestWithContext(ctx, req.Method, req.URL(), buf)
+	r, err := http.NewRequestWithContext(ctx, req.Method, req.URL(), reqBody)
 	if err != nil {
 		return nil, throw(err)
 	}
@@ -96,34 +110,36 @@ func (p *proxy) Run(ctx context.Context, input any) (any, error) {
 	if err != nil {
 		return nil, throw(err)
 	}
-	defer d.Body.Close()
+	//defer d.Body.Close()
 	resp := &Response{
 		StatusCode: d.StatusCode,
-		Header:     make(map[string]string),
+		Header:     make(map[string]any),
+		Body:       d.Body,
 	}
-	for k, v := range d.Header {
-		if len(v) > 0 {
-			resp.Header[k] = v[0] // 只取第一个值
-		}
-	}
-	body, err := io.ReadAll(d.Body)
-	if err != nil {
-		return nil, throw(err)
-	}
-	if len(body) > 0 {
-		contentType := d.Header.Get("Content-Type")
-		switch {
-		case strings.Contains(contentType, "application/json"):
-			if err := json.Unmarshal(body, &resp.Body); err != nil {
-				return nil, throw(err)
-			}
-		case strings.Contains(contentType, "application/x-yaml"):
-			if err := yaml.Unmarshal(body, &resp.Body); err != nil {
-				return nil, throw(err)
-			}
-		default:
-			return nil, throw(errors.New("unsupported response header Content-Type: " + contentType))
-		}
-	}
-	return plugin.EncodeOutput(resp)
+	return resp.Encode()
+	//for k, v := range d.Header {
+	//	if len(v) > 0 {
+	//		resp.Header[k] = v[0] // 只取第一个值
+	//	}
+	//}
+	//body, err := io.ReadAll(d.Body)
+	//if err != nil {
+	//	return nil, throw(err)
+	//}
+	//if len(body) > 0 {
+	//	contentType := d.Header.Get("Content-Type")
+	//	switch {
+	//	case strings.Contains(contentType, "application/json"):
+	//		if err := json.Unmarshal(body, &resp.Body); err != nil {
+	//			return nil, throw(err)
+	//		}
+	//	case strings.Contains(contentType, "application/x-yaml"):
+	//		if err := yaml.Unmarshal(body, &resp.Body); err != nil {
+	//			return nil, throw(err)
+	//		}
+	//	default:
+	//		return nil, throw(errors.New("unsupported response header Content-Type: " + contentType))
+	//	}
+	//}
+	//return plugin.EncodeOutput(resp)
 }
